5b03c4a21e8340c0299a65f734c7047e7e9c2289
DiawaraMariam <68479960+DiawaraMariam@users.noreply.github.com>
2025-09-04 11:04:46 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
+-- Example Queries
+
+-- List all patients with their appointments
+SELECT p.first_name, p.last_name, a.appointment_date, a.status
+FROM Patients p
+JOIN Appointments a ON p.patient_id = a.patient_id;
+
+-- Get doctor schedule
+SELECT d.first_name, d.last_name, a.appointment_date, p.first_name AS patient_name
+FROM Doctors d
+JOIN Appointments a ON d.doctor_id = a.doctor_id
+JOIN Patients p ON a.patient_id = p.patient_id;
 
