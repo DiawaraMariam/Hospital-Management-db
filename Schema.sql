15b6ed37524d7135565412068beae800d52fd1d9
DiawaraMariam <68479960+DiawaraMariam@users.noreply.github.com>
2025-09-04 00:07:32 -0400

    Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,1 +1,25 @@
 
+CREATE TABLE Departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Doctors (
+    doctor_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    specialization VARCHAR(50),
+    phone VARCHAR(15),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES Departments(department_id)
+);
+
+CREATE TABLE Patients (
+    patient_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    dob DATE,
+    gender CHAR(1),
+    phone VARCHAR(15),
+    address VARCHAR(100)
+);
