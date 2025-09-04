b0904e3b323398a5862b212ca7d224c54007b203
DiawaraMariam <68479960+DiawaraMariam@users.noreply.github.com>
2025-09-04 11:05:56 -0400

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,1 +1,11 @@
+-- Stored Procedures
+
+-- Get patient history
+CREATE PROCEDURE get_patient_history(IN pid INT)
+BEGIN
+    SELECT a.appointment_id, a.appointment_date, t.description, t.medication, t.cost
+    FROM Appointments a
+    JOIN Treatments t ON a.appointment_id = t.appointment_id
+    WHERE a.patient_id = pid;
+END;
 
